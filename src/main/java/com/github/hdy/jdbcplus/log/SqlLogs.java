8ac6252d98aef29b1f7ed267ce41030cc587a189
hdy10 <498218711@qq.com>
2019-11-30 11:47:14 +0800

    查询结果打印日志最多只显示500字符

--- src/main/java/com/github/hdy/jdbcplus/log/SqlLogs.java
+++ src/main/java/com/github/hdy/jdbcplus/log/SqlLogs.java
@@ -1,13 +1,18 @@
 package com.github.hdy.jdbcplus.log;
 
 
-
 import com.github.hdy.jdbcplus.util.Logs;
 
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 public class SqlLogs {
+    private final long period;
+    private final AtomicLong now;
 
     /**
      * @param sql
@@ -31,7 +36,7 @@ public class SqlLogs {
         Logs.info("┣查询类型: " + type);
         Logs.info("┣ S Q L : " + sql);
         Logs.info("┣查询参数: " + "[]");
-        Logs.info("┣返回数据: " + data.toString());
+        Logs.info("┣返回数据: " + (data.toString().length() > 500 ? data.toString().substring(0, 500) + "..." : data.toString()));
         Logs.info("┣处理时间: " + times + "ms");
         Logs.info("┗━━━━━ " + className + "." + methodName + "(" + fileName + ":" + lineNumber + ")");
     }
@@ -87,7 +92,39 @@ public class SqlLogs {
 
 
     public static long getCurrentTimeMillis() {
-        return System.currentTimeMillis();
+        return now();
+    }
+
+    public static long now() {
+        return instance().currentTimeMillis();
+    }
+
+    private long currentTimeMillis() {
+        return now.get();
+    }
+
+    private static SqlLogs instance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    private SqlLogs(long period) {
+        this.period = period;
+        this.now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(runnable -> {
+            Thread thread = new Thread(runnable, "System Clock");
+            thread.setDaemon(true);
+            return thread;
+        });
+        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), period, period, TimeUnit.MILLISECONDS);
+    }
+
+
+    private static class InstanceHolder {
+        public static final SqlLogs INSTANCE = new SqlLogs(1);
     }
 
     /**
